1ee82277a41f8f3db421ce3466045766b3d8a17e
pvl-ao <pvl-ao@pm.me>
2022-07-03 23:23:40 +0300

    Add script to open/close faq section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-'use strict'
+'use strict';
+
+const faq = document.querySelector('.faq');
+const faqToggle = document.querySelectorAll('.faq-toggle');
+
+// Open/Close FAQ
+faqToggle.forEach((element) => {
+  element.addEventListener('click', () => {
+    element.parentNode.classList.toggle('active');
+  });
+});
